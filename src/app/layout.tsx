26409e789590e3385b4231306debafe6ac06b91c
Satellite-system <adarshurmaliya@gmail.com>
2026-01-20 18:04:53 +0530

    Update layout.tsx-> app fix suprressed Warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn('min-h-screen bg-background font-sans antialiased', ptSans.variable)}>
+      <body suppressHydrationWarning className={cn('min-h-screen bg-background font-sans antialiased', ptSans.variable)}>
         <FirebaseClientProvider>
           {children}
           <Toaster />
