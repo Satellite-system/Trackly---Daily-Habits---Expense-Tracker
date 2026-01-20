34e70386af5aaf67fb79c5f72a67df4302939f2d
Divya chaturvedi <divyachaturvedi859@gmail.com>
2026-01-20 10:42:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3612735937-4ea62",
+  "appId": "1:969310819958:web:1a3658ab3f5618f9fa7157",
+  "apiKey": "AIzaSyBS4x4eOSWwfnYSl0APDucTI0N5uBpeO_0",
+  "authDomain": "studio-3612735937-4ea62.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "969310819958"
+};
